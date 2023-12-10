b13b80fea90e45ebc9a51c6558fe883f9e46c99f
matt-wid3 <matt@wid3.org>
2023-12-10 03:00:21 +0100

    added initial POC WIDE Server integration

--- src/app/services/claims-processor.service.ts
+++ src/app/services/claims-processor.service.ts
@@ -15,13 +15,29 @@ declare global {
 })
 export class ClaimsProcessorService {
   private web3: Web3 | null = null;
+  private connectedToWallet: boolean = false;
 
   constructor() {
-    if (window.ethereum) {
-      this.web3 = new Web3(window.ethereum);
-    } else {
+    this.connectedToWallet = this.initMetaMaskSetup();
+
+    if (!this.connectedToWallet) {
       console.error('MetaMask is not installed!');
     }
+  }
+
+  public isConnectedToWallet(): boolean {
+    return this.connectedToWallet;
+  }
+
+  public initMetaMaskSetup(): boolean {
+    if (window.ethereum) {
+      this.web3 = new Web3(window.ethereum);
+      this.connectedToWallet = true;
+    } else {
+      this.connectedToWallet = false;
+    }
+
+    return this.isConnectedToWallet();
   }
 
   public async connectWalletMetaMask(): Promise<void> {
@@ -38,6 +54,7 @@ export class ClaimsProcessorService {
     }
   }
 
+  //TODO: Sample implementation, not yet fully integrated
   public async connectWalletConnect(): Promise<void> {
     const provider = new WalletConnectProvider({
       infuraId: 'YOUR_INFURA_ID' // Replace with your Infura ID
@@ -50,13 +67,28 @@ export class ClaimsProcessorService {
     }
   }
 
+  public async getPublicKeys(): Promise<string[] | null> {
+    if (!this.web3) {
+      console.error('MetaMask is not available');
+      return null;
+    }
+
+    const accounts = await this.web3.eth.getAccounts();
+    if (accounts.length === 0) {
+      console.error('No accounts found');
+      return null;
+    }
+
+    return accounts;
+  }
+
   public async encryptData(data: string): Promise<any | null> {
     if (!this.web3) {
       console.error('Web3 is not initialized');
       return null;
     }
 
-    //await this.connectWalletConnect();
+    await this.connectWalletMetaMask();
 
     const accounts = await this.web3.eth.getAccounts();
     if (accounts.length === 0) {
@@ -72,6 +104,28 @@ export class ClaimsProcessorService {
       }
     } catch (error) {
       console.error('Error encrypting data', error);
+      return null;
+    }
+  }
+
+
+  public async signMessage(message: string): Promise<string | null> {
+    if (!this.web3) {
+      console.error('MetaMask is not available');
+      return null;
+    }
+
+    try {
+      const accounts = await this.web3.eth.getAccounts();
+      if (accounts.length === 0) {
+        throw new Error('No accounts found. Please connect to MetaMask.');
+      }
+
+      const account = accounts[0];
+      const signature = await this.web3.eth.personal.sign(message, account, '');
+      return signature;
+    } catch (error) {
+      console.error('Error signing message:', error);
       return null;
     }
   }
@@ -105,7 +159,7 @@ export class ClaimsProcessorService {
     }
   }
 
-  private async getEncryptionPublicKey(account: string): Promise<string | null> {
+  public async getEncryptionPublicKey(account: string): Promise<string | null> {
     if (!window.ethereum) {
       console.error('Ethereum object not found');
       return null;
