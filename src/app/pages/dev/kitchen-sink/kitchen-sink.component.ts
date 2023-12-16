3b2f8cb7d1ef3995a4e1f94318dbc0f641045420
matt-wid3 <matthew@wid3.org>
2023-12-17 00:44:25 +0100

    added input text and radio to kitchen sink

--- src/app/pages/dev/kitchen-sink/kitchen-sink.component.ts
+++ src/app/pages/dev/kitchen-sink/kitchen-sink.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-kitchen-sink',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './kitchen-sink.component.html',
   styleUrl: './kitchen-sink.component.scss'
 })
 export class KitchenSinkComponent {
+  textboxValue = '';
+  radioValue = '';
+  radioValueB = '';
+  checkboxValue = false;
 
   buttonClick(msg: string): void {
     alert(`Clicked ${msg}`);
