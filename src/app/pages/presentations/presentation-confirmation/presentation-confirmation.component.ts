415cacf47e4bc28d568741a2992b9d2bdbe84b36
matt-wid3 <matthew@wid3.org>
2024-01-09 23:16:25 +0100

    revised presentation flow, added tooltips

--- src/app/pages/presentations/presentation-confirmation/presentation-confirmation.component.ts
+++ src/app/pages/presentations/presentation-confirmation/presentation-confirmation.component.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-presentation-confirmation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './presentation-confirmation.component.html',
   styleUrl: './presentation-confirmation.component.scss'
 })
 export class PresentationConfirmationComponent {
-
+  presentation: any = {
+    "verifier": {
+      "name": "University of Malta"
+    },
+    "timestamp": new Date().getTime(),
+    "decrypted": [{"name": "profileId", "value": 123456}, {"name": "email", "value": "john.doe@nomail.net"}],
+    "encrypted": [{"name": "name", "value": "syOOkBKSTXXmrMTQ2N6UO0l18Z2kxVUhVg=="}, {"name": "surname", "value": "Kf4nJHVq+PX4MviNfcO02RK2XZRCI6ie"}]
+  }
   constructor() { }
 
   present() {
