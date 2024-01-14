806d6822e13bad2fd079045ae04d306d586d8fe0
matt-wid3 <matthew@wid3.org>
2024-01-14 01:54:02 +0100

    added multibutton component

--- src/app/pages/client/main/main.component.ts
+++ src/app/pages/client/main/main.component.ts
@@ -4,7 +4,7 @@ import { NavMenuComponent } from '../../../components/nav-menu/nav-menu.componen
 import { NavHeaderComponent } from '../../../components/nav-header/nav-header.component';
 import { NavMenuService } from '../../../services/nav-menu.service';
 import { Web3WalletService } from '../../../services/web3-wallet.service';
-import { Subscription, catchError, throwError } from 'rxjs';
+import { Subscription, catchError, firstValueFrom, throwError } from 'rxjs';
 import { EncryptedCredentialComponent } from '../../../components/table/encrypted-credential/encrypted-credential.component';
 import { Router } from '@angular/router';
 import { WideStorageService } from '../../../services/wide-storage.service';
@@ -28,7 +28,7 @@ export class MainComponent implements OnInit {
 
   //TODO: Strongly type, include in presentation components etc...
   issuedCredentials: any[] = [];
-  credentialDetailLookup: any = { };
+  credentialDetailLookup: any = {};
 
   constructor(private web3WalletService: Web3WalletService, private wideStorageService: WideStorageService, private toastNotificationService: ToastNotificationService, private navMenuService: NavMenuService, private router: Router) {
     this.walletSubscription = this.web3WalletService.connectedToWallet$.subscribe(walletConnected => {
@@ -77,44 +77,31 @@ export class MainComponent implements OnInit {
     this.router.navigate([uri]);
   }
 
-  expand(row: any) {
-    if (!this.account){
+  async expand(row: any) {
+    if (!this.account) {
       this.toastNotificationService.info('Unable to retrieve credential', 'Please make sure your wallet is connected first');
       return;
     }
 
     if (row.expanded === undefined) {
-      if (!this.credentialDetailLookup[row.wideInternalId]){
-        this.wideStorageService.getEncryptedCredentials(this.account, row.wideInternalId).subscribe({
-          next: (response: HttpResponse<any>) => {
-            //TODO: Define credential type for strong typing
-            this.credentialDetailLookup[row.wideInternalId] = response.body;
-    
-            if (response.status == 204) {
-              this.toastNotificationService.info('Loading credentials', `No credentials found for ${this.account} with key ${row.wideInternalId}`);
-            }
-          },
-          error: (res) => {
-            this.toastNotificationService.error(res.statusText, `Failed to load credentials (${res.status})`);
-            console.error('Failed to load credentials', res);
-          }
-        });;
+      //We call this to lazy load if not available
+      const credentials = await this.getCredentialsForIssuer(row.wideInternalId)
+      if (credentials) {
+        row.expanded = true;
       }
-      row.expanded = true;
     } else {
       row.expanded = !row.expanded;
     }
   }
 
   async decryptProperty(cred: any) {
-    console.info('decrypting property', cred);
     cred.isDecrypting = true;
-
     cred.status = 1;
+
     const response: any = await this.web3WalletService.decryptData(cred.val);
     const decryptedData: any = JSON.parse(response);
 
-    if (decryptedData && decryptedData.hasOwnProperty(cred.name)){
+    if (decryptedData && decryptedData.hasOwnProperty(cred.name)) {
       cred.decryptedValue = decryptedData[cred.name];
       cred.status = 2;
       this.toastNotificationService.info('Decryption Complete', `Successfully decrypted data for ${cred.name}`)
@@ -123,36 +110,88 @@ export class MainComponent implements OnInit {
       this.toastNotificationService.error('Failed Decrypting', `Unable to retrieve value from decrypted data for ${cred.name}`);
       console.error('Unable to retrieve value from decrypted data. Returned value: ', decryptedData);
     }
+
+    cred.isDecrypting = false;
   }
 
-  async decryptCred(cred: any) {
+  async decryptCredential(issuer: any) {
+    const issuerCredentialData = await this.getCredentialsForIssuer(issuer.wideInternalId);
 
-    //TODO: Continue here
-    console.info('decrypting credential', cred);
+    console.log(issuerCredentialData);
 
-    cred.isDecrypting = true;
-    cred.status = 1;
-    cred.props.forEach((prop: any) => {
-      prop.isDecrypting = true;
-      prop.status = 1;
+    if (!issuerCredentialData) {
+      this.toastNotificationService.error('Cannot decrypt', 'Unable to find any credentials for this issuer');
+      return;
+    }
+
+    issuerCredentialData.credentials.forEach((cred: any) => {
+      cred.isDecrypting = true;
+      cred.status = 1;
     });
 
-    await this.web3WalletService.signMessage('User will not be asked to sign message, but decrypt. This is just to simulate the flow');
+    const response: any = await this.web3WalletService.decryptData(issuerCredentialData.payload);
+    const decryptedData: any = JSON.parse(response);
 
-    cred.status = 2;
-    cred.props.forEach((prop: any) => {
-      prop.status = 2;
+    console.log(decryptedData);
+
+    issuerCredentialData.credentials.forEach((cred: any) => {
+      cred.decryptedValue = decryptedData[cred.name];
+      cred.isDecrypting = false;
+      cred.status = 2;
     });
 
-    console.info('decrypted successfully', cred);
+    this.toastNotificationService.info('Successful Decryption', 'Successfully decrypted all credentials');
+
+    ///////
+    
+    // await this.web3WalletService.signMessage('User will not be asked to sign message, but decrypt. This is just to simulate the flow');
+
+    // cred.status = 2;
+    // cred.props.forEach((prop: any) => {
+    //   prop.isDecrypting = false;
+    //   prop.status = 2;
+    // });
+
+    // cred.isDecrypting = false;
+    // console.info('decrypted successfully', cred);
   }
 
-  credHasDecryptPending(cred: any) {
-    if (cred.props && cred.props.length > 0) {
-      return cred.props.some((p: any) => p.status != 2);
+  issuerHasDecryptPending(issuer: any) {
+    if (!this.credentialDetailLookup[issuer.wideInternalId]) {
+      return true;
+    }
+
+    const credentials = this.credentialDetailLookup[issuer.wideInternalId].credentials;
+
+    if (credentials && credentials.length > 0) {
+      return credentials.some((c: any) => c.status != 2);
     }
 
     return false;
+  }
+
+  async getCredentialsForIssuer(issuerInternalId: string): Promise<any | null> {
+    if (!this.credentialDetailLookup[issuerInternalId] && this.account) {
+      try {
+        const response: HttpResponse<any> = await firstValueFrom(
+          this.wideStorageService.getEncryptedCredentials(this.account, issuerInternalId)
+        );
+  
+        this.credentialDetailLookup[issuerInternalId] = response.body;
+  
+        if (response.status === 204) {
+          this.toastNotificationService.info('Loading credentials', `No credentials found for ${this.account} with key ${issuerInternalId}`);
+        }
+  
+        return this.credentialDetailLookup[issuerInternalId];
+      } catch (error: any) {
+        this.toastNotificationService.error(error.statusText, `Failed to load credentials (${error.status})`);
+        console.error('Failed to load credentials', error);
+        return null;
+      }
+    } else {
+      return this.credentialDetailLookup[issuerInternalId];
+    }
   }
 
   getFaviconUrl(url: string): string {
--- src/app/pages/dev/kitchen-sink/kitchen-sink.component.ts
+++ src/app/pages/dev/kitchen-sink/kitchen-sink.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { WideDropdownComponent } from '../../../components/wide-dropdown/wide-dropdown.component';
 import { ToastNotificationService } from '../../../services/toast-notification.service';
 import { WideInputComponent } from '../../../components/wide-input/wide-input.component';
+import { MultiButtonComponent } from '../../../components/multi-button/multi-button.component';
 
 @Component({
   selector: 'app-kitchen-sink',
   standalone: true,
-  imports: [CommonModule, FormsModule, WideDropdownComponent, WideInputComponent],
+  imports: [CommonModule, FormsModule, WideDropdownComponent, WideInputComponent, MultiButtonComponent],
   templateUrl: './kitchen-sink.component.html',
   styleUrl: './kitchen-sink.component.scss'
 })
@@ -30,6 +31,10 @@ export class KitchenSinkComponent {
     alert(`Clicked ${msg}`);
   }
 
+  handleMultiButtonClick(action: string) {
+    console.log('Button action:', action);
+    // Add logic to handle different actions
+}
   showToast(type: string): void {
     this.toastNotificationService.showToast('Toast Title', 'Butter be prepared for some toasty puns!', type, 5000);
   }
