88c14bc3dfcf377d7732b1b3f91f4a2ce02509b1
matt-wid3 <matthew@wid3.org>
2024-01-17 20:26:55 +0100

    updated prod env to connect to deployed API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: false,
-    wideServerApiUrl: 'https://<PROD_API_URL>:<PORT>', // Production API URL
-    googleOAuth: {
-      clientId: 'TODO',
-      redirectUri: 'TODO',
-      oAuthUri: 'TODO'
-    }
-  };
+  production: false,
+  wideServerApiUrl: 'https://wide-api-test-4kg8i.ondigitalocean.app/',
+  googleOAuth: {
+    clientId: '708493994076-rlpvffondu5hee5vgmkfpn0b50fkl4ti.apps.googleusercontent.com',
+    redirectUri: 'https://wide-client-git-develop-wide.vercel.app/credentials/add/google/redirect',
+    oAuthUri: 'https://accounts.google.com/o/oauth2/v2/auth'
+  }
+};
