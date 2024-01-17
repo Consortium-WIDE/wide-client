78451e7ff421795d79a5c49b77e6ad69215fc80b
matt-wid3 <matthew@wid3.org>
2024-01-17 20:29:22 +0100

    fixed typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  wideServerApiUrl: 'https://wide-api-test-4kg8i.ondigitalocean.app/',
+  wideServerApiUrl: 'https://wide-api-test-4kg8i.ondigitalocean.app',
   googleOAuth: {
     clientId: '708493994076-rlpvffondu5hee5vgmkfpn0b50fkl4ti.apps.googleusercontent.com',
     redirectUri: 'https://wide-client-git-develop-wide.vercel.app/credentials/add/google/redirect',
