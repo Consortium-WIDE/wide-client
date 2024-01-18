0579e25f3f19a740e332e3ff79f75f4fffc3b1c9
matt-wid3 <matthew@wid3.org>
2024-01-18 01:25:24 +0100

    pushing dist folder for digital ocean deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   wideServerApiUrl: 'https://wide-api-test-4kg8i.ondigitalocean.app',
   googleOAuth: {
     clientId: '708493994076-rlpvffondu5hee5vgmkfpn0b50fkl4ti.apps.googleusercontent.com',
-    redirectUri: 'https://wide-client-git-develop-wide.vercel.app/credentials/add/google/redirect',
+    redirectUri: 'https://wide-api-test-4kg8i.ondigitalocean.app/credentials/add/google/redirect',
     oAuthUri: 'https://accounts.google.com/o/oauth2/v2/auth'
   }
 };
