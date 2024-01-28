4b96a2f416ccc100dede49c1fef67e204857445e
matt-wid3 <matthew@wid3.org>
2024-01-29 00:22:01 +0100

    changes to support presentation flow

--- src/app/pages/presentations/presentation-confirmation/presentation-confirmation.component.ts
+++ src/app/pages/presentations/presentation-confirmation/presentation-confirmation.component.ts
@@ -1,30 +1,86 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { WideModalComponent } from '../../../components/wide-modal/wide-modal.component';
+import { HttpClient } from '@angular/common/http';
+import { Web3WalletService } from '../../../services/web3-wallet.service';
+import { ToastNotificationService } from '../../../services/toast-notification.service';
 
 @Component({
   selector: 'app-presentation-confirmation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WideModalComponent],
   templateUrl: './presentation-confirmation.component.html',
   styleUrl: './presentation-confirmation.component.scss'
 })
-export class PresentationConfirmationComponent {
-  presentation: any = {
-    "verifier": {
-      "name": "University of Malta"
-    },
-    "timestamp": new Date().getTime(),
-    "decrypted": [{"name": "profileId", "value": 123456}, {"name": "email", "value": "john.doe@nomail.net"}],
-    "encrypted": [{"name": "name", "value": "syOOkBKSTXXmrMTQ2N6UO0l18Z2kxVUhVg=="}, {"name": "surname", "value": "Kf4nJHVq+PX4MviNfcO02RK2XZRCI6ie"}]
+export class PresentationConfirmationComponent implements OnInit {
+  pageLoaded: boolean = false;
+  processedCredential: any = null;
+  presentationConfig: any = null;
+  domainOrigin: string = '';
+  showModal: boolean = false;
+
+  constructor(private httpClient: HttpClient, private web3WalletService: Web3WalletService, private toastNotificationService: ToastNotificationService) { }
+
+  ngOnInit(): void {
+    console.log('history.state', history.state);
+
+    const { processedCredential, presentationConfig, domainOrigin } = history.state;
+    this.processedCredential = processedCredential;
+    this.presentationConfig = presentationConfig;
+    this.domainOrigin = domainOrigin;
+
+    this.pageLoaded = true;
   }
-  constructor() { }
 
   present() {
-    window.location.href =  "https://google.com/?q=present";
+    //1. Generate a token
+    const token = this.generateSecureToken(32);
+
+    //2. Log on RP Api
+    this.httpClient.post(this.presentationConfig.serverApiEndpoint, {
+      key: token,
+      data: this.processedCredential
+    }).subscribe({
+      next: (response) => {
+        //3. Redirect to redirectUri and include token
+        window.location.href = `${this.presentationConfig.redirectUri}?token=${token}`
+      },
+      error: (error) => {
+        // Handle the error here
+        console.error('Error uploading data', error);
+        this.toastNotificationService.error(error.statusText, `Failed to present credentials (${error.status})`);
+      },
+      complete: () => {
+        // Code to run on completion (if needed)
+        console.log('Request completed');
+      }
+    });
   }
 
   reject() {
-    window.location.href =  "https://google.com/?q=reject"
+    window.location.href = this.presentationConfig.sourceUri;
+  }
+
+  //TODO: place in a dedicated service
+  generateSecureToken(length: number): string {
+    const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let array = new Uint8Array(length);
+    window.crypto.getRandomValues(array);
+    let token = '';
+
+    for (let i = 0; i < array.length; i++) {
+      token += validChars.charAt(array[i] % validChars.length);
+    }
+
+    return token;
+  }
+
+  truncatePredicateValue(credential: any): string {
+    if (credential.type === 'proof') {
+      return (credential.value.slice(0, 32)) + '...';
+    }
+
+    return credential.value;
   }
 }
--- src/app/pages/presentations/presentation-request/presentation-request.component.ts
+++ src/app/pages/presentations/presentation-request/presentation-request.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { TooltipComponent } from '../../../components/tooltip/tooltip.component';
 import { CommonModule } from '@angular/common';
+import { WideStorageService } from '../../../services/wide-storage.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Web3WalletService } from '../../../services/web3-wallet.service';
+import { ToastNotificationService } from '../../../services/toast-notification.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-presentation-request',
@@ -14,8 +19,9 @@ export class PresentationRequestComponent implements OnInit {
   domainOrigin: string = '';
   presentationConfig: any = null;
   pageLoaded: boolean = false;
+  isProcessing: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private wideStorageService: WideStorageService, private httpClient: HttpClient, private web3WalletService: Web3WalletService, private toastNotificationService: ToastNotificationService) { }
 
   ngOnInit(): void {
     const historyState = history.state;
@@ -23,18 +29,194 @@ export class PresentationRequestComponent implements OnInit {
     this.domainOrigin = historyState.domainOrigin;
     this.presentationConfig = historyState.presentationConfig;
 
+    //TODO: Redirect and error if domain origin is not found;
+
     this.pageLoaded = true;
   }
 
   formatPredicates(predicates: any): any {
     return predicates.map((predicate: any) => {
-      const key = Object.keys(predicate)[0];
-      const details = predicate[key];
-      return `${key} ${details.op} '${details.value}'`;
+      return `${predicate.property} ${predicate.op} '${predicate.value}'`;
     });
   }
 
   reject() {
     window.location.href = this.presentationConfig.rejectUri ?? this.presentationConfig.sourceUri;
   }
+
+  async prepareRequest() {
+    if (this.web3WalletService.isConnectedToWallet()) {
+      const account = await this.web3WalletService.getAccount();
+      if (account) {
+        this.isProcessing = true;
+        //1. Get Issued Credentials
+        await this.wideStorageService.getUserIssuedCredentials(account).subscribe({
+          next: async (response: HttpResponse<any>) => {
+            const issuedCredentials = response.body;
+
+            if (response.status == 204) {
+              this.toastNotificationService.info('Loading credentials', `No credentials found for ${account}`);
+              this.isProcessing = false;
+            } else {
+              //2. Filter on credentials that meet required criteria
+              const matchingCredentialIssuers = this.findMatchingCredentials(issuedCredentials, this.presentationConfig);
+
+              if (matchingCredentialIssuers.length == 0) {
+                this.toastNotificationService.error("No credentials found", `No matching credentials found (${this.presentationConfig.credential.join(', ')})`);
+                this.isProcessing = false;
+                console.error(`No matching credentials found (${this.presentationConfig.credential.join(', ')})`);
+              } else {
+                if (matchingCredentialIssuers.length > 1) {
+                  this.toastNotificationService.info("Multiple credentials found", `Multiple credentials matching (${this.presentationConfig.credential.join(', ')}) found. This will be supported soon!`);
+                  this.isProcessing = false;
+                } else {
+                  //3. Fetch and decrypt payload
+                  const credentialIssuer = matchingCredentialIssuers[0];
+                  const encryptedCredential = await this.getCredentialsForIssuer(account, credentialIssuer.wideInternalId);
+                  const decryptedCredential = await this.decryptCredential(encryptedCredential.payload);
+
+                  //4. Process request
+                  const processedCredential = await this.processCredential(credentialIssuer, encryptedCredential, decryptedCredential);
+
+                  //5. Navigate to Review screen
+                  this.router.navigateByUrl('present/confirm', { state: { processedCredential: processedCredential, presentationConfig: this.presentationConfig, domainOrigin: this.domainOrigin } });
+                }
+              }
+            }
+          },
+          error: (res) => {
+            this.toastNotificationService.error(res.statusText, `Failed to load credentials (${res.status})`);
+            console.error('Failed to load credentials', res);
+            this.isProcessing = false;
+          }
+        });
+      } else {
+        this.toastNotificationService.error("Unexpected Error", `Failed to retrieve account address from MetaMask`);
+        console.error('Cannot retrieve account from metamask');
+      }
+    } else {
+      this.toastNotificationService.error("Unexpected Error", `Failed to connect to MetaMask`);
+      console.error('Not connected to metamask!');
+    }
+  }
+
+  async processCredential(issuer: any, encryptedCredential: any, decryptedCredential: any): Promise<any> {
+    const serverPubKey = await this.web3WalletService.getServerPublicKey();
+
+    let vc = {
+      "id": "http://wid3.xyz",
+      "type": ["WIDECredential"],
+      "issuer": serverPubKey,
+      "issuanceDate": new Date().toISOString(),
+      "credentialSubject": {
+        "id": issuer.credentialSubject.id,
+        "dataTypes": {
+          "type": "credentialSources",
+          "name": "Original Credential Sources",
+          "value": [issuer.type]
+        },
+        "requestedBy": this.presentationConfig.rpName,
+        "issuerDomains": [
+          {
+            "id": issuer.id,
+            "name": issuer.issuer,
+            "label": issuer.label,
+            "type": issuer.type,
+            "data": {
+              "payloadKeccak256CipherText": this.web3WalletService.hashTextKeccak256(encryptedCredential.payload.ciphertext),
+              "credentials": [] as any[]
+            }
+          }
+        ]
+      }
+    }
+
+    const requiredCredentialsPlainText = this.presentationConfig.require.plainText;
+    requiredCredentialsPlainText.forEach((c: any) => {
+      vc.credentialSubject.issuerDomains[0].data.credentials.push({
+        "name": c,
+        "value": decryptedCredential[c],
+        "type": "plainText"
+      });
+    });
+
+    const requiredCredentialsProof = this.presentationConfig.require.proofOf;
+    requiredCredentialsProof.forEach((c: any) => {
+      vc.credentialSubject.issuerDomains[0].data.credentials.push({
+        "name": c,
+        "value": this.web3WalletService.hashDataKeccak256(encryptedCredential.credentials.filter((encryptedCred: any) => encryptedCred.name == c)[0].val.ciphertext),
+        "type": "proof"
+      });
+    });
+
+    const requiredCredentialsPredicate = this.presentationConfig.require.predicate;
+    requiredCredentialsPredicate.forEach((c: any) => {
+      //TODO: Explore methods for allowing RP to verify without knowing the data
+      //e.g. record a hash of the original value, and predicate result, or by 
+      //asking the user to sign a message attesting to the predicate
+      vc.credentialSubject.issuerDomains[0].data.credentials.push({
+        "name": c.property,
+        "value": this.evalPredicate(c, decryptedCredential[c.property]),
+        "type": "predicate",
+        "predicate": `${c.property} ${c.op} '${c.value}'`
+      });
+    });
+
+    return vc;
+  }
+
+  //Very basic implementation for now that will be developed on a case by case basis
+  evalPredicate(predicateDefinition: any, decryptedValue: any): any {
+    switch (predicateDefinition.op) {
+      case 'endsWith':
+        return decryptedValue.endsWith(predicateDefinition.value);
+      default:
+        console.error('unable to evaluate predicate: ', predicateDefinition);
+        return false;
+    }
+  }
+  //TODO: Put in dedicated service.
+  findMatchingCredentials(issuedCredentials: any[], presentationConfig: any): any[] {
+    return issuedCredentials.filter(cred => {
+      // Convert all elements to lowercase and sort
+      const sortedCredTypes = cred.type.map((type: any) => type.toLowerCase()).sort();
+      const sortedPresentationCreds = presentationConfig.credential.map((cred: any) => cred.toLowerCase()).sort();
+
+      // Compare the JSON string representations of the sorted arrays
+      return JSON.stringify(sortedCredTypes) === JSON.stringify(sortedPresentationCreds);
+    });
+  }
+
+  //TODO: Duplicate code with main, put in dedicated service.
+  async decryptCredential(payload: any): Promise<any> {
+
+    const response: any = await this.web3WalletService.decryptData(payload);
+    const decryptedData: any = JSON.parse(response);
+
+    this.toastNotificationService.info('Successful Decryption', 'Successfully decrypted all credentials');
+
+    return decryptedData;
+  }
+
+  //TODO: Duplicate code with main, put in dedicated service.
+  async getCredentialsForIssuer(account: string, issuerInternalId: string): Promise<any | null> {
+    try {
+      const response: HttpResponse<any> = await firstValueFrom(
+        this.wideStorageService.getEncryptedCredentials(account, issuerInternalId)
+      );
+
+      const credentialDetail = response.body;
+
+      if (response.status === 204) {
+        this.toastNotificationService.info('Loading credentials', `No credentials found for ${account} with key ${issuerInternalId}`);
+      }
+
+      return credentialDetail;
+    } catch (error: any) {
+      this.toastNotificationService.error(error.statusText, `Failed to load credentials (${error.status})`);
+      console.error('Failed to load credentials', error);
+      return null;
+    }
+  }
+
 }
