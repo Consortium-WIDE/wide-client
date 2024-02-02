70142f81f997523b8ca375e5aa709cf1fc3915c8
matt-wid3 <matthew@wid3.org>
2024-02-02 16:29:39 +0100

    updated environment propertie and built for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,7 @@ export const environment = {
   wideServerApiUrl: 'https://api.test.wid3.app',
   googleOAuth: {
     clientId: '708493994076-rlpvffondu5hee5vgmkfpn0b50fkl4ti.apps.googleusercontent.com',
-    redirectUri: 'https://test.wid3.app/credentials/add/google/redirect',
-    oAuthUri: 'https://accounts.google.com/o/oauth2/v2/auth'
+    redirectUri: 'https://test.wid3.app/credentials/add/google/redirect'
   },
   microsoftOAuth: {
     msalConfig : {
