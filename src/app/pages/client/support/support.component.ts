53a1b38740fd7841b7ef04f52f39972578509a1f
matt-wid3 <matthew@wid3.org>
2024-02-09 14:30:46 +0100

    various design updates following internal workshop

--- src/app/pages/client/support/support.component.ts
+++ src/app/pages/client/support/support.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavMenuService } from '../../../services/nav-menu.service';
 
 @Component({
   selector: 'app-support',
@@ -8,6 +9,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './support.component.html',
   styleUrl: './support.component.scss'
 })
-export class SupportComponent {
+export class SupportComponent implements OnInit {
 
+  constructor(private navMenuService: NavMenuService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.navMenuService.setPageDetails('Support', []);
+  }
 }
